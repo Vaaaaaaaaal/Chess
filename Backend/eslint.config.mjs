1db78d7ba766de74d727838049be5a328ac4f15f
valentin <valentin.goux@etu.univ-lyon1.fr>
2025-01-15 15:10:23 +0100

    maj pck.json and eslint

--- Backend/eslint.config.mjs
+++ Backend/eslint.config.mjs
@@ -1,10 +1,18 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.node, // Puisque c'est un projet backend, on utilise node au lieu de browser
+      },
+    },
+  },
   ...tseslint.configs.recommended,
+  {
+    ignores: ["dist/**", "node_modules/**"], // Ignore les fichiers compilés et node_modules
+  },
 ];
